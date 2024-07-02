3cf56f711833d5c44cae5a0b62dd6c1756fda195
soraya1618 <sorayyakhawari@gmail.com>
2024-07-02 08:42:12 +0000

    02*-02-variablen.js

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -6,3 +6,44 @@
  * 
  * 
  */
+
+/*****Variablen 01 ******/
+
+//console.log("Hi");
+//console.log(Hi);
+
+/* Deklaration + wertzuweisung I */
+
+//let firstName; // Deklatration(definition)
+//console.log(firstName); //Ausgabe
+//firstName = "Max"; // wertzuweisung | assignment
+//console.log(firstName); //Ausgabe
+//let familyName = "Mütze"; // Deklaration + wertzuweisung
+//console.log("Hi, ich bin " + firstName + " " + familyName + "!"); 
+
+/*
+wertzuweisung II + Datentypen
+JS ist ein untypisierte Sprache!
+*/
+
+let test; //deklaration
+
+/* undefindred */
+console.log("Datentyp: " + typeof test);
+test = "hi";
+test = 2;
+
+console.log(test);
+
+
+
+
+
+// Variable
+let test ; // variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe
+
+
+
